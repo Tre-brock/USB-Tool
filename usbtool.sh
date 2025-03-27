e77400a5804efb8c2d9be39f618d3c501c6c3b79
Tre-brock <ebrocklesby77@gmail.com>
2025-03-27 19:57:59 -0400

    fixed a bug

--- usbtool.sh
+++ usbtool.sh
@@ -18,9 +18,9 @@ echo "USB Tool, Write iso to usb drive."
 echo ""
 echo "Put the iso location here"
 read iso
-sudo lsblk
+sudo lsblk -d
 echo "What drive to you want to write to (WILL DESTROY ALL DATA)"
 read disk
 sudo umount $disk
-sudo dd if=$iso of=$disk bs=4M status=progress oflag=sync
+sudo dd if=$iso of=/dev/$disk bs=4M status=progress oflag=sync
 sudo eject $disk
