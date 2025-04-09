2b72053780e715b8ed3b518c27e66a2a9450a4ee
Tre_brock <152460754+Tre-brock@users.noreply.github.com>
2025-04-09 00:16:53 +0000

    added windows version that works under cygwin

--- usbtoolwin.sh
+++ usbtoolwin.sh
@@ -13,14 +13,14 @@ echo "
    ██║   ██║   ██║██║   ██║██║     
    ██║   ╚██████╔╝╚██████╔╝███████╗
    ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝
-                                    "
-echo "UNIX - Version 1.2.2"
+                                   "
+echo "Windows - Version 1.2.2"
 echo ""
 echo "Put the iso location here"
 read iso
-sudo lsblk -d
-echo "What drive to you want to write to (WILL DESTROY ALL DATA)"
+cat /proc/partitions
+echo "What drive to you want to write to? (WILL DESTROY ALL DATA)"
+echo "UNDER THE COLUMN 'name', NOT THE WINDOWS NAME"
 read disk
 sudo umount $disk
 sudo dd if=$iso of=/dev/$disk bs=4M status=progress oflag=sync
-sudo eject $disk
