773d2790ee9596a3a746b8d1b4de168bbc15a270
Tre_brock <152460754+Tre-brock@users.noreply.github.com>
2025-04-09 00:17:59 +0000

    ?

--- usbtool.sh
+++ usbtool.sh
@@ -19,7 +19,7 @@ echo ""
 echo "Put the iso location here"
 read iso
 sudo lsblk -d
-echo "What drive to you want to write to (WILL DESTROY ALL DATA)"
+echo "What drive to you want to write to? (WILL DESTROY ALL DATA)"
 read disk
 sudo umount $disk
 sudo dd if=$iso of=/dev/$disk bs=4M status=progress oflag=sync
